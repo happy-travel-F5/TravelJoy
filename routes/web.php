809784a5b64c5fe80e32adba1ec8efab93269808
Jeaneth Sanchez Nuñez <jeaneth.sanchez@gmail.com>
2023-08-21 21:12:34 +0200

    destination page and controller working

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+use App\Http\Controllers\DestinationController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/destinations', [DestinationController::Class, 'index'])->name('destinations');
-   
-
+Route::get('/destinations', [DestinationController::class, 'index'])->name('destinations');
