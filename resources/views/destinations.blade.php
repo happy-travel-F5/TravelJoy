59685211dd971bd20faf0b6aa88b04c8b177d211
Jeaneth Sanchez Nuñez <jeaneth.sanchez@gmail.com>
2023-08-23 10:32:00 +0200

    header en destinations funcionando

--- resources/views/destinations.blade.php
+++ resources/views/destinations.blade.php
@@ -5,29 +5,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Destinations</title>
     <link rel="preconnect" href="https://fonts.bunny.net">
-    <link rel="stylesheet" href="{{ asset('css/destinations.css') }}">
-
-    
-
-    
+    <link rel="stylesheet" href="{{ asset('css/destinations.css') }}">  
 </head>
 
 <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
             @if (Route::has('login'))
                 <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
-                    @auth
-                        <a href="{{ url('/home') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Home</a>
-                    @else
-                        <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a>
-                        @endif
-                    @endauth
+                   
                 </div>
             @endif
 
 <body class="destinations">
+    @extends('layouts.app')
+    @section('content')
     @foreach ($destinations as $destination)
     <div class="destinations-image"> <img src="{{ $destination->image }}" alt="{{ $destination->title }}" width="100"></div>
     <!-- ... Rest of the HTML ... -->
@@ -36,5 +26,6 @@
     <div class="destinations-location">{{ $destination->location }}</div>
     <!-- ... Rest of the HTML ... -->
     @endforeach
+    @endsection
 </body>
 </html>
