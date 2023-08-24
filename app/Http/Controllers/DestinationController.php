6f5ee1416195d34139a5640bd497ea5ec099d7c8
Rosapedret2 <rosapedret2@gmail.com>
2023-08-24 14:40:27 +0200

    changes en controller

--- app/Http/Controllers/DestinationController.php
+++ app/Http/Controllers/DestinationController.php
@@ -9,7 +9,6 @@ class DestinationController extends Controller
         $destinations = Destination::all();
         return view('destinations', compact('destinations'));
     }
-<<<<<<< HEAD
     public function create()
     {
         return view('create');
@@ -68,56 +67,8 @@ public function destroy(int $id)
     return redirect()->route('destinations')->with('success', 'Destino eliminado exitosamente.');
 }
 
-=======
 
 
-//     /**
-//      * Show the form for creating a new resource.
-//      */
-//     public function create()
-//     {
-//         //
-//     }
-
-//     /**
-//      * Store a newly created resource in storage.
-//      */
-//     public function store(Request $request)
-//     {
-//         //
-//     }
-
-//     /**
-//      * Display the specified resource.
-//      */
-//     public function show(string $id)
-//     {
-//         //
-//     }
-
-//     /**
-//      * Show the form for editing the specified resource.
-//      */
-//     public function edit(string $id)
-//     {
-//         //
-//     }
-
-//     /**
-//      * Update the specified resource in storage.
-//      */
-//     public function update(Request $request, string $id)
-//     {
-//         //
-//     }
-
-//     /**
-//      * Remove the specified resource from storage.
-//      */
-//     public function destroy(string $id)
-//     {
-//         //
-//     }
 
 //Controler de search
 
@@ -128,17 +79,13 @@ public function search(Request $request)
 
     return view('destinations', ['destinations' => $destinations]);
 }
->>>>>>> feature/search
 }
 
 
 
-<<<<<<< HEAD
 
 
 
 
 
 
-=======
->>>>>>> feature/search
