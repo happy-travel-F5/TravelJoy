a6c3e9b804b95c5ee510b251cda05e15b5dceebd
Jeaneth Sanchez Nuñez <jeaneth.sanchez@gmail.com>
2023-08-24 12:53:12 +0200

    vinculación de todas las páginas y css editar y detail

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -1,19 +1,44 @@
 @extends('layouts.app')
-
+@section('styles')
+    <link href="{{ asset('css/detail.css') }}" rel="stylesheet">
+@endsection
 @section('content')
-    <div class="container">
-        <div class="card">
+    <div class="edit-container">
+        
             <img src="{{ $destination->image }}" class="card-img-top" alt="{{ $destination->title }}">
-            <div class="card-body">
-                <h5 class="card-title">{{ $destination->title }}</h5>
-                <p class="card-text"><strong>Localización:</strong> {{ $destination->location }}</p>
-                <p class="card-text"><strong>Motivo para viajar:</strong> {{ $destination->reason }}</p>
-                <a href="{{ route('destinations.edit', ['id' => $destination->id]) }}" class="btn btn-primary">Editar</a>
-                <form action="{{ route('destinations.destroy', ['id' => $destination->id]) }}" method="POST" style="display: inline-block">
+        <div class="edit-body">
+
+            <div class="edit-info-header">
+                <div class="edit-info">
+                    <h5 class="edit-title">{{ $destination->title }}</h5>
+                     <p class="edit-text"> {{ $destination->location }}</p>
+                </div>
+                
+                <div class="edit-buttons">
+                    <a href="{{ route('destinations.edit', ['id' => $destination->id]) }}"> <img src="{{ asset('images/Edit-icon.svg') }}" alt="Icono de editar" width="20"></a>
+                    
+                    <form action="{{ route('destinations.destroy', ['id' => $destination->id]) }}" method="POST" style="display: inline-block">
                     @csrf
                     @method('DELETE')
-                    <button type="submit" class="btn btn-danger">Eliminar</button>
-                </form>
+                    <button type="submit" class="button-edit">
+                    <img src="{{ asset('images/Delete-icon.svg') }}" width="18" alt="Eliminar" class="button-icon">
+                    </button>
+                    </form>
+                </div>
+                
+                
+        </div>
+       
+        <p class="edit-reason"><strong>Motivo para viajar:</strong> {{ $destination->reason }}</p>
+       
+            
+    </div>
+                
+
+                
+    
+                
+               
             </div>
         </div>
     </div>
