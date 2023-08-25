fcd027b693bfda89a73e53c491a74f13bcad5c5a
Jeaneth Sanchez Nuñez <jeaneth.sanchez@gmail.com>
2023-08-25 09:32:04 +0200

    ultimas correciones css

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,6 +1,5 @@
 @extends('layouts.app')
 
-<<<<<<< Updated upstream
 @section('styles')
     <link href="{{ asset('css/register.css') }}" rel="stylesheet">
 @endsection
@@ -21,24 +20,6 @@
 
                             <div class="input-container">
                                 <input id="email" type="email" class="custom-form-input @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus placeholder="Escribe tu email...">
-=======
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
->>>>>>> Stashed changes
 
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
@@ -48,19 +29,11 @@
                             </div>
                         </div>
 
-<<<<<<< Updated upstream
                         <div class="input-container">
                             <label for="password" class="custom-form-label">{{ __('Password') }}</label>
 
                             <div class="col-md-6">
                                 <input id="password" type="password" class="custom-form-input @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" placeholder="Escribe tu contraseña...">
-=======
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
->>>>>>> Stashed changes
 
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
@@ -70,7 +43,6 @@
                             </div>
                         </div>
 
-<<<<<<< Updated upstream
                        
                             
                         </div>
@@ -83,31 +55,6 @@
                                 <a class="cancel-button" href="{{ url('/') }}">Cancelar</a>
 
                                
-=======
-                        <div class="row mb-3">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
->>>>>>> Stashed changes
                             </div>
                         </div>
                     </form>
